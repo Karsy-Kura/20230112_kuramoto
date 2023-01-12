b23ab42210477cd4a0313737ef0aa2172fc6853f
Karsy-Kura <aksf.soccer.uverworld@gmail.com>
2023-01-12 18:09:51 +0900

    first commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+use App\Http\Requests\TodoRequest;
+
+class TodoController extends Controller
+{
+  public function index()
+  {
+    $todos = Todo::all();
+    return view('index', ['todos' => $todos]);
+  }
+
+  public function store(TodoRequest $request)
+  {
+    $new_todo = new Todo();
+    $form = $request->all();
+    unset($form['_token']);
+    $new_todo->fill($form)->save();
+    return redirect('/');
+  }
+
+  public function update(TodoRequest $request)
+  {
+    $todo = Todo::find($request->id);
+    $form = $request->all();
+    unset($form['_token']);
+    $todo->fill($form)->save();
+    return redirect('/');
+  }
+
+  public function delete(Request $request)
+  {
+    Todo::where('id', $request->id)->delete();
+    return redirect('/');
+  }
+}

7c8ec1f9111fbe652bcec3cb6f3b6a0a25175bb9
Karsy-Kura <aksf.soccer.uverworld@gmail.com>
2023-01-20 15:19:53 +0900

    install breeze

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,15 @@ use App\Http\Controllers\TodoController;
 |
 */
 
-Route::get('', [TodoController::class, 'index']);
-Route::group(['prefix' =>'todo'], function() {
+Route::get('/', [TodoController::class, 'index']);
+Route::group(['prefix' => 'todo'], function () {
   Route::post('create', [TodoController::class, 'store']);
   Route::post('update', [TodoController::class, 'update']);
   Route::post('delete', [TodoController::class, 'delete']);
 });
+
+Route::get('/dashboard', function () {
+  return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
